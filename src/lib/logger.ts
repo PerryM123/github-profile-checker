ca19afb9c755cd062d4859e79d80f180d4fb034e
PerryM123 <perry.frontend@gmail.com>
2026-03-07 20:35:04 +0900

    ログ出力のためにpinoを導入

---
+++ src/lib/logger.ts
@@ -0,0 +1,17 @@
+// src/lib/logger.ts
+import pino from 'pino'
+
+const logger = pino({
+  level: process.env.LOG_LEVEL || 'info',
+  ...(process.env.NODE_ENV === 'development' && {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'SYS:yyyy-mm-dd HH:MM:ss.l',
+      },
+    },
+  }),
+})
+
+export default logger
