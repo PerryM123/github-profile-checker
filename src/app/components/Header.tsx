f8d7a02903a5e8d2563f59832eb3955dc20ff408
PerryM123 <perrymiranda1@yahoo.com>
2026-03-08 18:31:54 +0900

    Headerを共通化するようにHeaderコンポーネントを作成

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="border-b border-gray-200 bg-white">
+      <div className="mx-auto max-w-7xl px-6 py-4">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-8">
+            <h1 className="text-xl font-bold text-black">GITHUB REPO SEARCH</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
 'use client'
 
 import { useState } from 'react'
+import Header from '@/app/components/Header'
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = useState('hello-world')
   return (
     <div className="min-h-screen bg-white">
-      <header className="border-b border-gray-200 bg-white">
-        <div className="mx-auto max-w-7xl px-6 py-4">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center space-x-8">
-              <h1 className="text-xl font-bold text-black">
-                GITHUB REPO SEARCH
-              </h1>
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header />
     </div>
   )
 }
