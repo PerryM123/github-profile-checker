8898d5fa0d55b4c2e01b6d762b6b6edf0dd75ce1
PerryM123 <perrymiranda1@yahoo.com>
2026-03-08 18:47:38 +0900

    HeaderロゴはLinkになるように修正

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link'
+
 export default function Header() {
   return (
     <header className="border-b border-gray-200 bg-white">
       <div className="mx-auto max-w-7xl px-6 py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-8">
-            <h1 className="text-xl font-bold text-black">GITHUB REPO SEARCH</h1>
+            <Link href="/" className="transition-all hover:opacity-80">
+              <h1 className="text-xl font-bold text-black">
+                GITHUB REPO SEARCH
+              </h1>
+            </Link>
           </div>
         </div>
       </div>
