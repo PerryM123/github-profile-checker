6da8d379a4ac579b182e0e8334cf1f456fbb18df
PerryM123 <perrymiranda1@yahoo.com>
2026-03-08 18:35:49 +0900

    Headerコンポーネントをlayoutに移動

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 'use client'
 
 import { useState } from 'react'
-import Header from '@/app/components/Header'
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = useState('hello-world')
-  return (
-    <div className="min-h-screen bg-white">
-      <Header />
-    </div>
-  )
+  return <div className="min-h-screen bg-white"></div>
 }
